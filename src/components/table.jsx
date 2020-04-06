d960ad3d73780d367dee5eb5a3af08a87831071d
Andrew Duda <dudanian@gmail.com>
2020-04-06 18:27:50 +0900

    table: memoize the rows and fix warnings
    React really wants thead to have a tr.
    
    Also memoized the rows, although they still rerender a lot for example
    when changing the category. Can probably further optimize.

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -3,36 +3,37 @@ import React from 'react';
 import images from '../../public/images/*.png';
 
 
-function FishRow(props) {
+const FishRow = React.memo(function(props) {
     return (
         <tr>
-            <td>{ props.fish.name }</td>
+            <td>{props.fish.name}</td>
             <td><img
-                src={ images[props.fish.image] }
-                alt={ props.fish.name } />
+                src={images[props.fish.image]}
+                alt={props.fish.name} />
             </td>
-            <td>{ props.fish.price }</td>
-            <td>{ props.fish.location }</td>
-            <td>{ props.fish.shadow }</td>
-            <td>{ props.fish.time }</td>
+            <td>{props.fish.price}</td>
+            <td>{props.fish.location}</td>
+            <td>{props.fish.shadow}</td>
+            <td>{props.fish.time}</td>
         </tr>
     );
-}
-
+});
 
 export default function FishTable(props) {
     return (
         <table>
             <thead>
-                <th>Name</th>
-                <th>Image</th>
-                <th>Price</th>
-                <th>Location</th>
-                <th>Shadow</th>
-                <th>Time</th>
+                <tr>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Location</th>
+                    <th>Shadow</th>
+                    <th>Time</th>
+                </tr>
             </thead>
             <tbody>
-                { props.data.map((v => <FishRow key={v.name} fish={v} />)) }
+                {props.data.map((v => <FishRow key={v.name} fish={v} />))}
             </tbody>
         </table>
     );
