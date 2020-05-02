d86e1c9add13070dc05f645ae4883207b81f466a
Andrew Duda <dudanian@gmail.com>
2020-05-03 00:07:34 +0900

    scrape: add support for bugs
    Next big change is to show the bug data as well. Moved around some
    functions and such to better support fish and bugs. Also some jank was
    added..

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -6,8 +6,8 @@ import requests
 from collections import OrderedDict
 
 
-URL = "https://animalcrossing.fandom.com/wiki/Fish_(New_Horizons)"
-
+FISH_URL = "https://animalcrossing.fandom.com/wiki/Fish_(New_Horizons)"
+BUGS_URL = "https://animalcrossing.fandom.com/wiki/Bugs_(New_Horizons)"
 
 ## helper functions
 def format_time(time):
@@ -54,7 +54,7 @@ def get_months(elems):
     return months
 
 def get_fish(elems):
-    return {
+    entry = {
         'name': get_text(elems[0]).title(),
         'image': get_link(elems[1]),
         'price': get_int(elems[2]),
@@ -64,6 +64,26 @@ def get_fish(elems):
         'months': get_months(elems[6:]),
     }
 
+    # XXX not sure if this is right
+    # but I don't want to include 'River (Clifftop) Pond'
+    if entry['location'] == 'River (Clifftop) Pond':
+        entry['location'] = 'River (Clifftop)'
+
+    if entry['location'] == 'Sea(while raining)':
+        entry['location'] = 'Sea (Raining)'
+
+    return entry
+
+def get_bug(elems):
+    entry = {
+        'name': get_text(elems[0]).title(),
+        'image': get_link(elems[1]),
+        'price': get_int(elems[2]),
+        'location': get_text(elems[3]),
+        'time': format_time(get_text(elems[4])),
+        'months': get_months(elems[5:]),
+    }
+    return entry
 
 ## filepaths
 curdir = os.path.dirname(__file__)
@@ -71,45 +91,50 @@ pubdir = os.path.join(curdir, '..', 'public')
 imgdir = os.path.join(pubdir, 'images')
 os.makedirs(imgdir, exist_ok=True)
 
+## parse tables
+def get_entries(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    tables = soup.find_all("div" ,class_="tabbertab")
 
-## script start
-page = requests.get(URL)
-soup = BeautifulSoup(page.text, 'html.parser')
-tables = soup.find_all("div" ,class_="tabbertab")
+    data = OrderedDict()
 
-data = OrderedDict()
-for table in tables:
-    hemisphere = table['title'].split()[0]
-    
-    # ignore first 2 entries
-    for tr in table.find_all('tr')[2:]:
-        fish = get_fish(tr.find_all('td'))
-        name = format_name(fish['name'])
-        months = fish['months']
+    for table in tables:
+        hemisphere = table['title'].split()[0]
 
-        # XXX not sure if this is right
-        # but I don't want to include 'River (Clifftop) Pond'
-        # there's also probably a much cleaner way to do this
-        if fish['location'] == 'River (Clifftop) Pond':
-            fish['location'] = 'River (Clifftop)'
+        for tr in table.find_all('tr'):
+            elems = tr.find_all('td')
+            entry = None
+            if len(elems) == 18:
+                entry = get_fish(elems)
+            elif len(elems) == 17:
+                entry = get_bug(elems)
+            else:
+                # ignore other rows
+                continue
+            name = format_name(entry['name'])
 
-        if fish['location'] == 'Sea(while raining)':
-            fish['location'] = 'Sea (Raining)'
+            # insert (or join)
+            months = entry['months']
+            if not name in data:
+                data[name] = entry
+                entry['months'] = {}
+            data[name]['months'][hemisphere] = months
 
-        # fix months for hemisphere
-        if not name in data:
-            data[name] = fish
-            fish['months'] = {}
-        data[name]['months'][hemisphere] = months
+            # download image if necessary
+            imgpath = os.path.join(imgdir, name+'.png')
+            if not os.path.exists(imgpath):
+                img = requests.get(entry['image'])
+                with open(imgpath, 'wb') as outfile:
+                    outfile.write(img.content)
+            entry['image'] = name
 
-        # download image if necessary
-        imgpath = os.path.join(imgdir, name+'.png')
-        if not os.path.exists(imgpath):
-            img = requests.get(fish['image'])
-            with open(imgpath, 'wb') as outfile:
-                outfile.write(img.content)
-        fish['image'] = name
+    return data
 
-jsonpath = os.path.join(pubdir, 'fish.json')
-with open(jsonpath, 'w') as outfile:
-    json.dump(list(data.values()), outfile, separators=(',', ':'))
+def write_json(data, filename):
+    jsonpath = os.path.join(pubdir, filename)
+    with open(jsonpath, 'w') as outfile:
+        json.dump(list(data.values()), outfile, separators=(',', ':'))
+
+write_json(get_entries(FISH_URL), 'fish.json')
+write_json(get_entries(BUGS_URL), 'bugs.json')
